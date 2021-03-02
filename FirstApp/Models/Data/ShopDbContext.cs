ce329d1d768fe483fe55f97c340f229e2fdb8bed
Javilingys <shkola.govno@gmail.com>
2021-03-02 15:54:17 +0700

    4. DbContext + Connection string

---
+++ FirstApp/Models/Data/ShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FirstApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Models.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,11 +1,10 @@
+using FirstApp.Models.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FirstApp
 {
@@ -13,7 +12,25 @@ namespace FirstApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<ShopDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
