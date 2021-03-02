17d821c1075b0c9a3957d22d942ac458ac343ca4
Javilingys <shkola.govno@gmail.com>
2021-03-02 16:26:25 +0700

    5. Database created from migration

---
+++ FirstApp/Models/Config/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using FirstApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Models.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(200);
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
--- FirstApp/Models/Data/ShopDbContext.cs
+++ FirstApp/Models/Data/ShopDbContext.cs
@@ -3,16 +3,42 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace FirstApp.Models.Data
 {
     public class ShopDbContext : DbContext
     {
+        // Products Table
         public DbSet<Product> Products { get; set; }
 
         public ShopDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        // Configure Enteties
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // FirstApp/Models/Config - Directory for configurations
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // конвертирует все в дабл. Как работает не очень понял.
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties()
+                    .Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name)
+                        .HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
