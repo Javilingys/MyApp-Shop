55dadb098999ded92e1c750b8e4ab4bbefe4501a
Javilingys <shkola.govno@gmail.com>
2021-03-04 21:46:44 +0700

    13. Update Product

--- FirstApp.Domain/Entities/Product.cs
+++ FirstApp.Domain/Entities/Product.cs
@@ -7,5 +7,9 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
     }
 }
