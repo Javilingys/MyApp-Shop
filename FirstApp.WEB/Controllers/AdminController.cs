50df3199602483c669a98cace482f152cfecc5ba
Javilingys <shkola.govno@gmail.com>
2021-03-12 15:29:12 +0700

    24. list all products into amdin panel

--- FirstApp.WEB/Controllers/AdminController.cs
+++ FirstApp.WEB/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,16 @@ namespace FirstApp.WEB.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IProductService _productService;
 
-
-        public IActionResult Index()
+        public AdminController(IProductService productService)
         {
-            return View();
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productService.GetProductsAsync());
         }
     }
 }
